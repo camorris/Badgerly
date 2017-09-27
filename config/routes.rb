155411ab1d5c84ce2cf0c910783b41b66f020b35
Chase Morris <camorris@callutheran.edu>
2017-09-26 17:56:21 -0700

    created user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions,  only: [:new, :create]
   delete 'logout' => 'sessions#destroy', as: :logout
-  resources :units
+  resources :badges
   
   # get 'users/index'
 
